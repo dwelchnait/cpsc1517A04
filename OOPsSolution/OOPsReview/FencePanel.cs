3fede9f5e70f3895e37cb8560e4ae4dc97355b2b
Don Welch <dwelch@nait.ca>
2019-09-09 16:27:32 -0600

    Review class properties

--- OOPsSolution/OOPsReview/FencePanel.cs
+++ OOPsSolution/OOPsReview/FencePanel.cs
@@ -13,5 +13,75 @@ namespace OOPsReview
 
     public class FencePanel
     {
+        //Properties
+        //a property is associated with a single piece of data
+        //a property has two sub components
+        //    get: returns the data value to the outside user
+        //    set: receive a data value and store it
+        //a property does NOT have parameter
+        //the set of the property using a keyword (value) to
+        //    hold the incoming data value
+
+        //Auto-implemented property
+        //does NOT need a data member coded
+        //the system will create an internal data storage element
+        //    equivalent to the property return datatatype
+        //no additional processing
+        //public double Height { get; set; } one could put data validation inside a property
+
+        //validation: must be greater than 0.0 and less than or equal to 8.0
+        //validation requires a fully implemented property
+        private double _Height;
+        public double Height
+        {
+            get
+            {
+                return _Height;
+            }
+            set
+            {
+                if (value > 0.0 && value <= 8.0)
+                {
+                    _Height = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Height");
+                }
+            }
+        }
+        public double Width { get; set; }
+
+        //Fully-implemented property
+        //does NEED a data member coded, usually private
+        //these properties usually contain additional logical processing
+        //this processing could be validation, ensuring appropriate data stored
+
+        //example: the style of fence is a string that either has characters
+        //         or no data (null)
+        private string _Style;
+        public string Style
+        {
+            get
+            {
+                return _Style;
+            }
+            set
+            {
+                //incoming data is located in the keyword value
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Style = null;
+                }
+                else
+                {
+                    _Style = value;
+                }
+            }
+        }
+
+        //handling nullable numerics
+        //only two possibilities: a) a numeric or b)null
+        public double? Price { get; set; }
     }
 }
