c7a408f534b46fe136b3ba1637f86f3e6d622c03
Don Welch <dwelch@nait.ca>
2019-09-11 16:53:48 -0600

    create Estimate class
    using classes within classes

---
+++ OOPsSolution/OOPsReview/Estimate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public class Estimate
+    {
+        public double TotalPrice { get; private set; }
+        public double LinearLength { get; set; }
+
+        public FencePanel Panel { get; set; }
+
+        public List<Gate> Gates { get; set; }
+
+        public void CalculateTotalPrice()
+        {
+            //assumption: you would put appropriate validation
+            //            at required points in your logic
+            //            to ensure data is present before using
+            double numberofpanels = Panel.EstimatedNumberOfPanels(LinearLength);
+            if((int)(numberofpanels * 10.0) > ((int)numberofpanels * 10))
+            {
+                numberofpanels = (int)numberofpanels + 1;
+            }
+            if(Panel.Price == null)
+            {
+                throw new Exception("Panel price is needed to calculate estimate");
+            }
+            else
+            {
+                TotalPrice += numberofpanels * (double)Panel.Price;
+                foreach(var item in Gates)
+                {
+                    TotalPrice += item.Price;
+                }
+            }
+            //return TotalPrice;
+        }
+
+    }
+}
