6dbe67d49419e70643a4db41fde1146f0157837f
Don Welch <dwelch@nait.ca>
2019-11-20 16:51:01 -0700

    customize gridview, paging, row selection, pass data

--- CSNet/WebApp/SamplePages/SqlQuery.aspx.cs
+++ CSNet/WebApp/SamplePages/SqlQuery.aspx.cs
@@ -74,5 +74,42 @@ namespace WebApp.SamplePages
                 }
             }
         }
+
+        protected void ProductList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //treat the Gridview as if it were an indexd table of rows
+            //columns which have ITemplates with web controls can be accessed via
+            //     the web control id value
+
+            //create a local alias to point to the table row of interest
+            GridViewRow agvrow = ProductList.Rows[ProductList.SelectedIndex];
+
+            //individual columns on a gridview can be accessed DEPENDING on how they
+            //    were defined in the gridview
+            //syntax   (agvrow.FindControl("xxxx") as controltype).controltypeaccess
+            //   where agvrow is the selected row
+            //         xxxx is the control on the gridview row using the ID attribute
+            //         controltype is the control type (Label, Textbox, CheckBox, DropdownList, ...)
+            //         controltypeaccess is how the particular control type is accessing
+            //data is returned as a string
+            string productid = (agvrow.FindControl("ProductID") as Label).Text;
+
+            //pass the data to our ReceivingPage
+            Response.Redirect("ReceivingPage.aspx?pid=" + productid);
+        }
+
+        protected void ProductList_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            //events usually come with a set of arguments
+            //the particular class of arguments are found in the event header
+            //different events have different argument classes
+
+            //you must set the gridview pageindex to the new page index carried by the e instance
+            ProductList.PageIndex = e.NewPageIndex;
+
+            //you must refresh your data collection and assign it to the control
+            Fetch_Click(sender, new EventArgs());
+
+        }
     }
 }
