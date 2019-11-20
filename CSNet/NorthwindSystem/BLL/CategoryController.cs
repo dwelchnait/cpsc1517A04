6c246883fa9d0dd0d6e4c7ee61c2bde231afb7d5
Don Welch <dwelch@nait.ca>
2019-11-20 16:51:59 -0700

    revert filter search implementation

--- CSNet/NorthwindSystem/BLL/CategoryController.cs
+++ CSNet/NorthwindSystem/BLL/CategoryController.cs
@@ -20,18 +20,5 @@ namespace NorthwindSystem.BLL
                 return context.Categories.ToList();
             }
         }
-        #region Filter Search Demo Interface
-        public List<Category> Category_List()
-        {
-            //need to connect to the Context class
-            //this connection will be done in a transaction coding group
-            using (var context = new NorthwindContext())
-            {
-                //via EnityFrame, make a request for all records,
-                //all attributes from the specified DbSet property
-                return context.Categories.ToList();
-            }
-        }
-        #endregion
     }
 }
--- CSNet/NorthwindSystem/DAL/NorthwindContext.cs
+++ CSNet/NorthwindSystem/DAL/NorthwindContext.cs
@@ -36,7 +36,5 @@ namespace NorthwindSystem.DAL
         public DbSet<Product> Products { get; set; }
         public DbSet<Region> Regions { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Supplier> Suppliers { get; set; }
-        public DbSet<Territory> Territories { get; set; }
     }
 }
